784f9f3d0ec1393b4b19554f830fb1a87f31256b
rushabhsr <sarvaiya.rushabh@gmail.com>
2024-09-19 11:42:33 +0530

    Aliases

--- commFuncParams.sh
+++ commFuncParams.sh
@@ -12,18 +12,22 @@ alias start_php='sudo systemctl start apache2 && sudo systemctl start mysql'
 # Aliases to stop Apache2 and MySQL
 alias stop_php='sudo systemctl stop apache2 && sudo systemctl stop mysql'
 
-alias sf='if [ "$(basename "$PWD")" != "frontend" ]; then cd frontend; fi && npm run dev'
-alias sb='if [ "$(basename "$PWD")" != "backend" ]; then cd backend; fi && npm run dev'
-alias sb-indi="cd ~/applications/indi-wheel/backend && sudo docker-compose up"
-alias sf-indi="cd ~/applications/indi-wheel/frontend && npm run dev"
-alias rd="npm run dev"
-
 # Function to create aliases for each folder in ~/applications
+APPS_DIR=$HOME/applications
 create_app_aliases() {
-  local app_dir=~/applications
-  for dir in "$app_dir"/*/; do
+  for dir in "$APPS_DIR"/*/; do
     folder_name=$(basename "$dir")
     alias "$folder_name"="cd \"$dir\""
+    alias "sf-$folder_name"="cd \"$dir\"frontend && rd"
+    alias "sb-$folder_name"="cd \"$dir\"backend && rd"
   done
 }
 create_app_aliases
+
+alias lapps="cd ${APPS_DIR}/ && ls -lrth"
+alias ltapps="cd ${APPS_DIR}/temp/ && ls -lrth"
+alias rd="npm run dev"
+alias sf='if [ "$(basename "$PWD")" != "frontend" ]; then cd frontend; fi && rd'
+alias sb='if [ "$(basename "$PWD")" != "backend" ]; then cd backend; fi && rd'
+alias sb-indi="cd ${APPS_DIR}/indi-wheel/backend && sudo docker-compose up"
+alias sf-indi="cd ${APPS_DIR}/indi-wheel/frontend && npm run dev"
